ad5fc74517ab140ad16e6f8d4425cfd4d26e490a
ricard0g <ricardoguzman444@gmail.com>
2024-08-15 03:44:20 -0400

    Finds Doctor by specialty using the FindDoctorSearch element

---
+++ src/components/FindDoctorSearch/FindDoctorSearch.js
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+import "./FindDoctorSearch.css";
+import { useNavigate, Navigate } from "react-router-dom";
+import doctorSearchImage from '../../assets/casual-life-3d-boy-online-doctor.png'
+
+const initSpeciality = [
+    "Dentist",
+    "Gynecologist/obstetrician",
+    "General Physician",
+    "Dermatologist",
+    "Ear-nose-throat (ent) Specialist",
+    "Homeopath",
+    "Ayurveda",
+];
+
+const FindDoctorSearch = () => {
+    const [doctorResultHidden, setDoctorResultHidden] = useState(true);
+    const [searchDoctor, setSearchDoctor] = useState("");
+    const [specialities, setSpecialities] = useState(initSpeciality);
+    const navigate = useNavigate();
+    const handleDoctorSelect = (speciality) => {
+        setSearchDoctor(speciality);
+        setDoctorResultHidden(true);
+        navigate(`/instant-consultation?speciality=${speciality}`);
+        window.location.reload();
+    };
+    return (
+        <div className="finddoctor">
+            <center>
+                <h1>Find a doctor and Consult instantly</h1>
+                <div className="img-container">
+                    {" "}
+                    <img src={doctorSearchImage} alt="Doctor 3D illustration searching on a computer" className="doctor-img" />
+                </div>{" "}
+                <div
+                    className="home-search-container"
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <div className="doctor-search-box">
+                        <input
+                            type="text"
+                            className="search-doctor-input-box"
+                            placeholder="Search doctors, clinics, hospitals, etc."
+                            onFocus={() => setDoctorResultHidden(false)}
+                            onBlur={() => setDoctorResultHidden(true)}
+                            value={searchDoctor}
+                            onChange={(e) => setSearchDoctor(e.target.value)}
+                        />
+
+                        <div className="findiconimg">
+                            <svg
+                                width="20px"
+                                height="20px"
+                                viewBox="0 0 32 32"
+                                version="1.1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="#000000"
+                            >
+                                <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+                                <g
+                                    id="SVGRepo_tracerCarrier"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+
+                                <g id="SVGRepo_iconCarrier">
+                                    {" "}
+                                    <title>search</title> <desc>Created with Sketch Beta.</desc>{" "}
+                                    <defs> </defs>{" "}
+                                    <g
+                                        id="Page-1"
+                                        stroke="none"
+                                        stroke-width="1"
+                                        fill="none"
+                                        fill-rule="evenodd"
+                                    >
+                                        {" "}
+                                        <g
+                                            id="Icon-Set"
+                                            transform="translate(-256.000000, -1139.000000)"
+                                            fill="#000000"
+                                        >
+                                            {" "}
+                                            <path
+                                                d="M269.46,1163.45 C263.17,1163.45 258.071,1158.44 258.071,1152.25 C258.071,1146.06 263.17,1141.04 269.46,1141.04 C275.75,1141.04 280.85,1146.06 280.85,1152.25 C280.85,1158.44 275.75,1163.45 269.46,1163.45 L269.46,1163.45 Z M287.688,1169.25 L279.429,1161.12 C281.591,1158.77 282.92,1155.67 282.92,1152.25 C282.92,1144.93 276.894,1139 269.46,1139 C262.026,1139 256,1144.93 256,1152.25 C256,1159.56 262.026,1165.49 269.46,1165.49 C272.672,1165.49 275.618,1164.38 277.932,1162.53 L286.224,1170.69 C286.629,1171.09 287.284,1171.09 287.688,1170.69 C288.093,1170.3 288.093,1169.65 287.688,1169.25 L287.688,1169.25 Z"
+                                                id="search"
+                                            >
+                                                {" "}
+                                            </path>{" "}
+                                        </g>{" "}
+                                    </g>{" "}
+                                </g>
+                            </svg>
+                        </div>
+                        <div
+                            className="search-doctor-input-results"
+                            hidden={doctorResultHidden}
+                        >
+                            {specialities.map((speciality) => (
+                                <div
+                                    className="search-doctor-result-item"
+                                    key={speciality}
+                                    onMouseDown={() => handleDoctorSelect(speciality)}
+                                >
+                                    <span>
+                                        <svg
+                                            width="20px"
+                                            height="20px"
+                                            viewBox="0 0 32 32"
+                                            version="1.1"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="#000000"
+                                        >
+                                            <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+                                            <g
+                                                id="SVGRepo_tracerCarrier"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                            />
+
+                                            <g id="SVGRepo_iconCarrier">
+                                                {" "}
+                                                <title>search</title>{" "}
+                                                <desc>Created with Sketch Beta.</desc> <defs> </defs>{" "}
+                                                <g
+                                                    id="Page-1"
+                                                    stroke="none"
+                                                    stroke-width="1"
+                                                    fill="none"
+                                                    fill-rule="evenodd"
+                                                >
+                                                    {" "}
+                                                    <g
+                                                        id="Icon-Set"
+                                                        transform="translate(-256.000000, -1139.000000)"
+                                                        fill="#000000"
+                                                    >
+                                                        {" "}
+                                                        <path
+                                                            d="M269.46,1163.45 C263.17,1163.45 258.071,1158.44 258.071,1152.25 C258.071,1146.06 263.17,1141.04 269.46,1141.04 C275.75,1141.04 280.85,1146.06 280.85,1152.25 C280.85,1158.44 275.75,1163.45 269.46,1163.45 L269.46,1163.45 Z M287.688,1169.25 L279.429,1161.12 C281.591,1158.77 282.92,1155.67 282.92,1152.25 C282.92,1144.93 276.894,1139 269.46,1139 C262.026,1139 256,1144.93 256,1152.25 C256,1159.56 262.026,1165.49 269.46,1165.49 C272.672,1165.49 275.618,1164.38 277.932,1162.53 L286.224,1170.69 C286.629,1171.09 287.284,1171.09 287.688,1170.69 C288.093,1170.3 288.093,1169.65 287.688,1169.25 L287.688,1169.25 Z"
+                                                            id="search"
+                                                        >
+                                                            {" "}
+                                                        </path>{" "}
+                                                    </g>{" "}
+                                                </g>{" "}
+                                            </g>
+                                        </svg>
+                                    </span>
+                                    <span>{speciality}</span>
+                                    <span>SPECIALITY</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </center>
+        </div>
+    );
+};
+
+export default FindDoctorSearch;
